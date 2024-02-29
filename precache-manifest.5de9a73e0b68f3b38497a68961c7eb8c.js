9e0f3039ef2fa97d6a5d3ff582035a64237c24f6
Jovanna <jovannadelatrinidad@gmail.com>
2024-02-29 15:58:32 -0800

    Updates

---
+++ precache-manifest.5de9a73e0b68f3b38497a68961c7eb8c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb68918e1ca1b10902728050a3a97ea",
+    "url": "/sightsensoryassist/index.html"
+  },
+  {
+    "revision": "077814e9d6fdaae5d769",
+    "url": "/sightsensoryassist/static/css/main.8a51b495.chunk.css"
+  },
+  {
+    "revision": "ca4458a6d1def7cbf965",
+    "url": "/sightsensoryassist/static/js/2.86ac9994.chunk.js"
+  },
+  {
+    "revision": "6aea8f39c38554b25bc6",
+    "url": "/sightsensoryassist/static/js/3.78ea13c0.chunk.js"
+  },
+  {
+    "revision": "077814e9d6fdaae5d769",
+    "url": "/sightsensoryassist/static/js/main.cff91fd0.chunk.js"
+  },
+  {
+    "revision": "4ec6f751fa5a27fcc987",
+    "url": "/sightsensoryassist/static/js/runtime~main.0d16326d.js"
+  },
+  {
+    "revision": "545fe55059c631f9dd974e9d1b574d07",
+    "url": "/sightsensoryassist/static/media/IMG_3832.545fe550.jpg"
+  },
+  {
+    "revision": "bd1fd39364266405e8dfbef1016f1e0d",
+    "url": "/sightsensoryassist/static/media/eye-removebg-preview.bd1fd393.png"
+  },
+  {
+    "revision": "bbac7d7c4782f0edb769bd4f14e61d9e",
+    "url": "/sightsensoryassist/static/media/group-photo.bbac7d7c.jpg"
+  },
+  {
+    "revision": "3ab3e1fbc674a38508668225f97314fe",
+    "url": "/sightsensoryassist/static/media/prototype.3ab3e1fb.jpeg"
+  },
+  {
+    "revision": "0397750178916d3a0b67754bc9661aa4",
+    "url": "/sightsensoryassist/static/media/sm2-removebg-preview.03977501.png"
+  }
+]);
